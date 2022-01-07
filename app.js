c3e0625eef502d88d0562842691cf70f6b956b07
TiagoFialho02 <96552523+TiagoFialho02@users.noreply.github.com>
2022-01-07 17:28:11 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 var express = require('express'); 
 const requestHandlers = require("./www/scripts/request-handlers");
 var fs = require("fs");
+var axios = require("axios").default;
+const { response } = require('express');
+require("dotenv").config();
+
 const app = express();
+
+var options = {
+    method: 'GET',
+    url: 'https://animalbreedapi.herokuapp.com/api/breeds',
+    headers:{
+        'API_KEY' : process.env.API_KEY,
+        '' : ''
+    }
+}
 
 app.get('/', function (request, response) {
     fs.readFile(__dirname + "/www/index.html", function(err, data){
@@ -14,6 +27,12 @@ app.get('/getAnimals', requestHandlers.getAnimals);
 app.get('/getUsers', requestHandlers.getUsers);
 
 app.get('/getPosts', requestHandlers.getPosts);
+
+axios.request(options.then(function (responde){
+    console(response.data);
+}).catch(function(error){
+    console.log(error);
+}))
 
 app.listen(8080, function () {
     console.log("Server running at http://localhost:8080");
