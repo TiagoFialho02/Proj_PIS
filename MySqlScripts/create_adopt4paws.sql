e61417bc10b6f77b5ea62abaab0bd8e9b670f314
TiagoFialho02 <96552523+TiagoFialho02@users.noreply.github.com>
2022-02-11 16:37:43 +0000

    updateSQL

--- MySqlScripts/create_adopt4paws.sql
+++ MySqlScripts/create_adopt4paws.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Fev-2022 às 18:46
+-- Tempo de geração: 11-Fev-2022 às 17:35
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.0
 
@@ -51,11 +51,14 @@ INSERT INTO `animals` (`id`, `type`, `breed`, `age`, `gender`, `photo`, `name`, 
 (3, 'Dog', 'Chihuahua', 1, 'Male', 'http://10.0.2.2:8080/getProfileImage/animalplaceholder.jpg', 'Guga', 64),
 (4, 'Dog', 'Beagle', 3, 'Female', 'http://10.0.2.2:8080/getProfileImage/animalplaceholder.jpg', 'Kiara', 63),
 (5, 'Cat', 'Siamese', 2, 'Female', 'http://10.0.2.2:8080/getProfileImage/animalplaceholder.jpg', 'Mantorras', 63),
-(6, 'Dog', 'Yorkshire', 0.3, 'Female', 'http://10.0.2.2:8080/getProfileImage/animalplaceholder.jpg', 'Yasmin', 3),
-(15, 'Cat', 'American shorthair', 2, 'Female', 'http://10.0.2.2:8080/getProfileImage/c9c12340-89d5-11ec-bad7-47d111f896a0Screenshot_20220128-041547.png', 'Luna', 1),
-(16, 'Cat', 'American shorthair', 2, 'Male', 'http://10.0.2.2:8080/getProfileImage/1fa3adf0-89d6-11ec-bad7-47d111f896a0IMG_20220115_000910.jpg', 'Totó', 3),
+(15, 'Cat', 'American shorthair', 2, 'Female', 'http://10.0.2.2:8080/getProfileImage/c9c12340-89d5-11ec-bad7-47d111f896a0Screenshot_20220128-041547.png', 'Luna', 2),
 (19, 'Cat', 'American curl', 3, 'Male', 'http://10.0.2.2:8080/getProfileImage/animalplaceholder.jpg', '123', 64),
-(26, 'Dog', 'Chihuahua', 4, 'Male', 'http://10.0.2.2:8080/getProfileImage/400fcd50-8a10-11ec-8052-cf6329e42a3dIMG_20220115_000937.jpg', 'Black', 67);
+(26, 'Dog', 'Chihuahua', 4, 'Male', 'http://10.0.2.2:8080/getProfileImage/400fcd50-8a10-11ec-8052-cf6329e42a3dIMG_20220115_000937.jpg', 'Black', 67),
+(27, 'Dog', 'Finnish Spitz', -1, 'Female', 'http://10.0.2.2:8080/getProfileImage/b518b380-8aa3-11ec-b265-15c518f9f675Screenshot_20220128-040136.png', 'Estrela', 64),
+(28, 'Dog', 'Yorkshire Terrier', 3, 'Male', 'http://10.0.2.2:8080/getProfileImage/fef978d0-8ac7-11ec-8530-8fb837e89fb7IMG_20220115_000937.jpg', 'Nico', 73),
+(29, 'Cat', 'Siamese', 5, 'Female', 'http://10.0.2.2:8080/getProfileImage/animalplaceholder.jpg', 'Riscas', 2),
+(30, 'Dog', 'Aidi', 5, 'Male', 'http://10.0.2.2:8080/getProfileImage/animalplaceholder.jpg', 'Ted', 2),
+(31, 'Dog', 'Airedale Terrier', 6, 'Male', 'http://10.0.2.2:8080/getProfileImage/animalplaceholder.jpg', 'Tadeu', 2);
 
 -- --------------------------------------------------------
 
@@ -74,9 +77,9 @@ CREATE TABLE `favorites` (
 --
 
 INSERT INTO `favorites` (`id`, `id_user`, `id_post`) VALUES
-(32, 64, 2),
 (35, 67, 17),
-(36, 67, 1);
+(36, 67, 1),
+(56, 73, 1);
 
 -- --------------------------------------------------------
 
@@ -100,7 +103,12 @@ CREATE TABLE `posts` (
 INSERT INTO `posts` (`id`, `id_user`, `id_animal`, `is_active`, `description`, `pub_date`) VALUES
 (1, 64, 2, b'1', 'Um cão muito bonito e também muito meigo.', '2022-02-04'),
 (2, 63, 4, b'1', 'Cão bastante meigo e bonito. Pode estar ao lado de crianças.', '2022-02-04'),
-(17, 67, 26, b'1', 'Cão do Gonçalo                                                                                                                                                      ', '2022-02-10');
+(17, 67, 26, b'1', 'Cão do Gonçalo                                                                                                                                                      ', '2022-02-10'),
+(18, 64, 27, b'1', 'Pequena, brincalhona e muito meiga. Gosta de crianças e não reage mal a desconhecidos', '2022-02-10'),
+(22, 2, 15, b'1', 'jnbasdiugbajsdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', '2022-02-11'),
+(23, 2, 29, b'1', 'redcvghvgvhbbjhjhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh\n\n\n', '2022-02-11'),
+(24, 2, 30, b'1', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n', '2022-02-11'),
+(25, 2, 31, b'1', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n', '2022-02-11');
 
 -- --------------------------------------------------------
 
@@ -128,17 +136,15 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `birthdate`, `profile_image`, `is_enterprise`, `is_preferences_set`, `p_type`, `p_age`, `p_gender`, `p_breed`) VALUES
-(1, 'mariamariana@gmail.com', 'MariaM', 'mariamaria', '1990-01-08', '0', 1, 0, NULL, NULL, '', '0'),
-(2, 'zecaAntonio@gmail.com', 'ZecaAn', '', '1989-01-22', '0', 1, 0, NULL, NULL, '', '0'),
-(3, 'joaquim123@gmail.com', 'Joaquim1', '', '1996-05-04', '0', 0, 0, NULL, NULL, '', '0'),
-(4, 'jecasmalacuec33o@gmail.co', 'Jecas123', '', '1994-08-07', '0', 0, 0, NULL, NULL, '', '0'),
-(5, '2200matias@gmail.com', 'Matias', '', '1980-07-01', '0', 1, 0, NULL, NULL, '', '0'),
+(1, 'mariamariana@gmail.com', 'MariaM', 'mariamaria', '1990-01-08', 'http://10.0.2.2:8080/getProfileImage/profile_placeholder.jpg', 1, 1, 'dog', 'Age', 'Male', 'Yorkshire Terrier'),
+(2, 'zecaAntonio@gmail.com', 'ZecaAn', '123456', '1989-01-22', 'http://10.0.2.2:8080/getProfileImage/profile_placeholder.jpg', 1, 1, 'dog', '-1', 'Male', 'Breed'),
 (63, '1234@gmail.com', '1234', '12341234', '1998-01-22', 'http://10.0.2.2:8080/getProfileImage/c76a7580-8073-11ec-8768-13ec6fe2e9b1IMG_20220115_000910.jpg', 0, 1, 'dog', 'Age', 'Female', 'Breed'),
 (64, 'admin@admin.com', 'admin', 'adminadmin', '1978-01-25', 'http://10.0.2.2:8080/getProfileImage/profile_placeholder.jpg', 0, 1, 'Type', 'Age', 'Gender', 'Breed'),
 (65, '12341@gmail.com', '12341', '12342134', '1997-01-28', 'http://10.0.2.2:8080/getProfileImage/c76a7580-8073-11ec-8768-13ec6fe2e9b1IMG_20220115_000910.jpg', 0, 1, 'cat', '1-2', 'Female', 'Siamese'),
 (66, '12341234@gmail.com', '12341234', '12341234', '1991-01-28', 'http://10.0.2.2:8080/getProfileImage/c76a7580-8073-11ec-8768-13ec6fe2e9b1IMG_20220115_000910.jpg', 0, 1, 'dog', '3-5', 'Female', 'Buldogue'),
 (67, 'gonzalo@gmail.com', 'Gonzalo', 'gonzalo', '2001-12-19', 'http://10.0.2.2:8080/getProfileImage/profile_placeholder.jpg', 0, 1, 'dog', '1-2', 'Gender', 'Breed'),
-(71, '12341@gmail.com', '12341234', '12341234', '1997-02-10', 'http://10.0.2.2:8080/getProfileImage/profile_placeholder.jpg', 0, 1, 'dog', '3-5', 'Male', 'Breed');
+(73, '123411@gmail.com', '1234123', '12341234', '1975-02-10', 'http://10.0.2.2:8080/getProfileImage/profile_placeholder.jpg', 0, 1, 'dog', '1-2', 'Gender', 'Breed'),
+(75, 'erica@gmail.com', 'erica2', '123456', '2001-02-02', 'http://10.0.2.2:8080/getProfileImage/profile_placeholder.jpg', 0, 1, 'both', '+5', 'Gender', 'Breed');
 
 --
 -- Índices para tabelas despejadas
@@ -176,7 +182,8 @@ ALTER TABLE `posts`
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
-  ADD UNIQUE KEY `email` (`email`,`username`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -186,25 +193,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `animals`
 --
 ALTER TABLE `animals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de tabela `favorites`
 --
 ALTER TABLE `favorites`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
 
 --
 -- AUTO_INCREMENT de tabela `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- Restrições para despejos de tabelas
