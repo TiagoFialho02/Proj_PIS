cb6aeb47e20e9f60130f165d82ebee7265caa58e
TiagoFialho02 <96552523+TiagoFialho02@users.noreply.github.com>
2022-02-21 15:30:04 +0000

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,6 @@ app.put('/api/updateAnimal', requestHandlers.updateAnimal);
 app.delete('/api/deleteUser', requestHandlers.deleteUser);
 
 function verifyJWT(req, res, next){
-    
     const token = req.headers['x-access-token'];
     if (!token) return res.status(401).json({ 
         auth: false, message: 'No token provided.' 
